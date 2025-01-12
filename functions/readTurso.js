38776326f005b2da96553083199df0a02e50aff6
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2025-01-12 18:25:57 -0500

    Add files via upload

--- functions/readTurso.js
+++ functions/readTurso.js
@@ -18,6 +18,7 @@ exports.handler = async (event, context) => {
   console.log('18aaa querystring parameters:')
   console.log(qsParms)
   console.log(qsParms.tblNm)
+  console.log(qsParms.make)
   zingo = 'Washburn'
   console.log('20 setting mySqlCmd:')
   // let mySqlCmd = 'SELECT * FROM guitars WHERE  make = "Washburn" '
