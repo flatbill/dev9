4a14325e6e9b2011a7bc8e7a5ab6d0d41b08a019
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2025-01-13 18:15:50 -0500

    Add files via upload

--- functions/readTurso.js
+++ functions/readTurso.js
@@ -1,12 +1,4 @@
-
 import { createClient } from "@libsql/client"
-
-// import { createClient } from '@supabase/supabase-js' 
-// const supaUrl = process.env.supaUrl
-// const supaAnonKey = process.env.supaAnonKey
-// const supabaseClient = createClient(supaUrl,supaAnonKey)
-//  
-
 exports.handler = async (event, context) => {
   console.log('11 running Netlify lambda function: readTurso')
   let turso1 = createClient({
@@ -19,19 +11,10 @@ exports.handler = async (event, context) => {
   console.log(qsParms)
   console.log(qsParms.tblNm)
   console.log(qsParms.make)
-  // zingo = 'Washburn'
-
-// for (const [key, value] of Object.entries(qsParms)) {
-//   console.log('iterating qsParms........')
-//   console.log(key)
-//   console.log( value)
-// }
-
   console.log('27 setting mySqlCmd:')
-  // let mySqlCmd = 'SELECT * FROM guitars WHERE  make = "Washburn" '
   let mySqlCmd = 'SELECT * FROM '  + qsParms.tblNm + ' WHERE '
   let mySqlCmdPart2 = ' '
-    for (const [key, value] of Object.entries(qsParms)) {
+  for (const [key, value] of Object.entries(qsParms)) {
       if (key!= 'tblNm'){
         console.log('iterating qsParms........')
         mySqlCmdPart2 +=  key + ' = '
@@ -39,46 +22,30 @@ exports.handler = async (event, context) => {
         + ' and '
         console.log(mySqlCmdPart2)
       }// end if
-    }
+  }//end for
   mySqlCmdPart2 = mySqlCmdPart2.slice(0, -5) // removes extra ' and '
   mySqlCmd += mySqlCmdPart2
   console.log('40 my new sql command:')
   console.log(mySqlCmd)
-  // + "'" + qsParms.make + "'" 
-
-    // "SELECT * FROM " + tbl +
-    // " WHERE " + keyFldNm  + ' = ' + keyFldVal 
   console.log('24 mySqlCmd:')
   console.log(mySqlCmd)
-  // let res = await this.turso.execute(mySqlCmd)
   let res =  await turso1.execute(mySqlCmd)
   console.log('33 done awaiting turso execute.')
-
   console.log('28 res:')
   console.table(res)
-    console.log('res.columns:')
-    console.log(res.columns)
-    console.log(res.columns[0])
-    console.log('res.rows.length:')
-    console.log(res.rows.length)
-    console.log('res.rows[0]:')
-    console.log(res.rows[0] )
-    // console.log('loop thru rows:')
-    // for (let x=0;x<res.rows.length;x++){
-    //   console.log(res.rows[x]['make'])
-    //   console.log(res.rows[x]['tone'])
-    //   // if (tblNm=='qtQuestions'){
-    //   //   console.log(res.rows[x]['id'])
-    //   //   console.log(res.rows[x]['questTxt'])
-    //   // } 
-    // }//end for
+  console.log('res.columns:')
+  console.log(res.columns)
+  console.log(res.columns[0])
+  console.log('res.rows.length:')
+  console.log(res.rows.length)
+  console.log('res.rows[0]:')
+  console.log(res.rows[0] )
 
   let myResponse = {
     statusCode: 200,
     headers: {'Access-Control-Allow-Origin': '*'},
     body:  JSON.stringify(res.rows)
   }
-  // body:  JSON.stringify('ratsyMatsy')
 
   console.log('22 readTurso.js   myResponse.body:')
   console.log(myResponse.body)
