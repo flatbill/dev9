ae65960be24f50d6fc57bfb4f3426c1533029fdc
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2025-01-14 12:42:38 -0500

    Add files via upload

--- functions/addTurso.js
+++ functions/addTurso.js
@@ -15,7 +15,7 @@ exports.handler = async (event, context) => {
   let mySqlCmdPart3 = ' ) VALUES ( '
   let mySqlCmdPart4 = ' '
   let mySqlCmdPart5 = ' )'
-  // let mySqlCmdPart6 = '; SELECT last_insert_rowid(); COMMIT; '
+  lastInsertRowId = 0
       
   for (const [key, value] of Object.entries(qsParms)) {
     if (key!= 'tblNm'){
@@ -30,29 +30,15 @@ exports.handler = async (event, context) => {
   console.log('34 my plussed-up sql command:')
   console.log(mySqlCmd)
   let res =  await turso1.execute(mySqlCmd)
-  // let lastRowInserted = await turso1.execute('SELECT last_insert_rowid()')
-
   console.log('33 done awaiting turso execute.')
-  // console.log(lastRowInserted)
-  // console.log('28 res:')
-  console.table(res)
-  console.log(res.lastInsertRowid )
-  // console.log('res.columns:')
-  // console.log(res.columns)
-  // console.log(res.columns[0])
-  // console.log('res.rows.length:')
-  // console.log(res.rows.length)
-  // console.log('res.rows[0]:')
-  // console.log(res.rows[0] )
+  console.log('34 res:')
+  console.table(res) //res has info about the newly inserted row.
 
   let myResponse = {
     statusCode: 200,
     headers: {'Access-Control-Allow-Origin': '*'},
     body:  JSON.stringify(res)
   }
-
-  // console.log('22 addTurso.js   myResponse.body:')
-  // console.log(myResponse.body)
   console.log('we reached the end of aaTurso.js. ready to return.')
   return myResponse   
 } // end export.handler 
