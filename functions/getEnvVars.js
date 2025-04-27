4a50c915984f75e695496615c36b876585a557f9
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2025-04-27 09:47:23 -0400

    Add files via upload

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -1,8 +1,10 @@
 
 exports.handler = async (event) => {
+  let whichDb = 'noDatabase'
+
   goodOriginsArray = [
     'flytechfree.com',
-    'bulmabarebones.netlify.app'
+    'yybulmabarebones.netlify.app'
   ]
   let originn =  event.headers.origin
   let originnMatch = ''
@@ -12,20 +14,20 @@ exports.handler = async (event) => {
   originnMatch = goodOriginsArray.filter(item => originn.includes(item))  
   if (originnMatch.length > 0){
     console.log('origin matched.'+ originn)
+    whichDb = process.env.whichDb
   }
   console.log('originn:')
   console.log(originn)
-  let whichDb = process.env.whichDb
-  console.log('4 process.env.whichDb:')
+  console.log('21 whichDb:')
   console.log(whichDb)
 
   let mossy = process.env.MOSS_MASTER
   console.log('mossy:')
   console.log(mossy)
-  console.log('reached line 22 of getEnvVars.js lambda')
+  console.log('reached line 27 of getEnvVars.js lambda')
   let response = {
       statusCode: 200,
-      headers: {'Access-Control-Allow-Origin': '*'},
+      headers: {'Access-Control-Allow-Origin': '*'}, //billy fix?
       body: JSON.stringify(whichDb)
   }
   console.log('24 end of getEnvVars.js')
