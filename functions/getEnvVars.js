21891a7f065d3021756a6bd11e50606734cbad7d
Bill Selzer <42444080+flatbill@users.noreply.github.com>
2025-04-27 09:57:13 -0400

    Add files via upload

--- functions/getEnvVars.js
+++ functions/getEnvVars.js
@@ -1,21 +1,17 @@
 
 exports.handler = async (event) => {
-  let whichDb = 'noDatabase'
-
+  let whichDb = '==noDatabase==' //default, maybe overWrite below.
   goodOriginsArray = [
-    'flytechfree.com',
+    'yyflytechfree.com',
     'yybulmabarebones.netlify.app'
   ]
   let originn =  event.headers.origin
   let originnMatch = ''
-  // const partialMatches = array.filter(item => item.includes("an")); // returns ["banana"]
-  // originnMatch = goodOriginsArray.filter(item => item.includes(originn))  
-  // originnMatch = goodOriginsArray.filter(item => item.includes('bulmabarebones'))  
   originnMatch = goodOriginsArray.filter(item => originn.includes(item))  
   if (originnMatch.length > 0){
     console.log('origin matched.'+ originn)
-    whichDb = process.env.whichDb
-  }
+    whichDb = process.env.whichDb //important. netlify env var.
+  }//end if
   console.log('originn:')
   console.log(originn)
   console.log('21 whichDb:')
@@ -24,12 +20,12 @@ exports.handler = async (event) => {
   let mossy = process.env.MOSS_MASTER
   console.log('mossy:')
   console.log(mossy)
-  console.log('reached line 27 of getEnvVars.js lambda')
+  console.log('reached line 23 of getEnvVars.js lambda')
   let response = {
       statusCode: 200,
-      headers: {'Access-Control-Allow-Origin': '*'}, //billy fix?
+      headers: {'Access-Control-Allow-Origin': '*'},
       body: JSON.stringify(whichDb)
   }
-  console.log('24 end of getEnvVars.js')
+  console.log('29 end of getEnvVars.js')
   return response
 };
